8b6e81fbd7a86a9184d524c95994a51a76345f24
njoroge33 <johngichuhi769@gmail.com>
2020-01-22 11:33:06 +0300

    add a user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient ) { }
+
+  registerNewUser(userData) {
+    return this.http.post('http://127.0.0.1:8000/api/users/', userData)
+  }
+}
